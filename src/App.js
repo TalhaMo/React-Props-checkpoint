f22518c82f8d04ce73cacf0f7d162db65318f72b
talhamo <talhamohamed23@yahoo.com>
2020-08-24 17:12:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Profile from './profile/Profile component/Profile';
+import image from './image.jpg';
 import './App.css';
 
+
+
 function App() {
+  
+const handleName =(name)=>{alert(`Name of profile user is ${name}`)}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Profile 
+  fullName="Talha Med Hossein" 
+  bio="I'm a mechanical enginneer graduated from National School of Engineering of Tunisia since 2015. Currently i want to jumpstart my career in software engineering." 
+  profession="Mechanical engineer" 
+   handleName={handleName}
+   >
+  
+  <img src={image} alt="myPicture" />
+
+  </Profile>
     </div>
   );
 }
---
+++ src/profile/Profile component/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Profile = (props) => {
+    return (
+        <div>  
+            <button type="button" class="btn btn-lg btn-success" onClick={()=>props.handleName(props.fullName)} >Alert user</button>
+            <p > <span>Full name:</span>  {props.fullName}</p>
+            <p><span>Profession:</span> {props.profession}</p>
+            <p><span>Bio:</span> {props.bio}</p>
+            <div>
+            {props.children}
+            </div>
+        </div>
+    )
+}
+Profile.defaultProps = {
+    fullName: 'Slim',
+    bio:' I\'m student for a software developer',
+    profession:'Software developer'
+    };
+
+Profile.propTypes = {
+    fullName: PropTypes.string,
+    bio: PropTypes.string,
+    profession: PropTypes.string,
+    handleName: PropTypes.func.isRequired
+} 
+
+export default Profile;
